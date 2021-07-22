f9516df40cd88a77ebe0edeb7ef40c86a35d529f
AGRawlins <83105062+agrawlins@users.noreply.github.com>
2021-07-21 23:13:46 -0400

    Added new components to allow for more OOP!

---
+++ app.jsx
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+import TitleBar from './TitleBar/titleBar';
+import BookViewer from './BookViewer/bookViewer';
+
+class App extends Component {
+    constructor(props){
+        super(props);
+        this.books = [
+            {title: 'Ready Player One', author: 'Ernest Cline'},
+            {title: 'All the Light We Cannot See', author: 'Anthony Doerr'},
+            {title: 'The First and Last Freedom', author: "Jiddu Krishnamurti"}
+        ];
+    this.state = {
+        bookNumber: 0
+        }
+    }
+
+    goToNextBook(){
+        let tempBookNumber = this.state.bookNumber;
+        tempBookNumber++;
+        if(tempBookNumber === this.books.length){
+            tempBookNumber = 0;
+        }
+        this.setState({
+            bookNumber: tempBookNumber
+        });
+    }
+
+    goToPreviousBook(){
+        let tempBookNumber = this.state.bookNumber;
+        tempBookNumber--;
+        if(tempBookNumber < 0)
+            tempBookNumber = this.books.length -1;
+        this.setState({
+            bookNumber: tempBookNumber
+        });
+    }
+    render() {
+        return(
+            <div className="container-fluid">
+                <TitleBar />
+                <BookViewer book={this.books[this.state.bookNumber]} nextBook={() => this.goToNextBook()} previousBook={() => this.goToPreviousBook()}/>
+            </div>
+        );
+    }
+}
+
+export default App;
